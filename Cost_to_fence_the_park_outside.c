a6270695a623eb8f3f6365e9645bdf7d1a5868b4
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-02-18 22:01:03 +0530

    #include<stdio.h> int main() {     int l,b,l1,b1,w,c,f;     scanf("%d%d%d%d",&l,&b,&w,&c);     l1=l+2*w;     b1=b+2*w;     f=((l1*b1)-(l*b))*c;    if(l1<=0||b1<=0)    {    printf("impossible");    }    else    {    printf("%d",f);    }     return 0;
    }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,l1,b1,w,c,f;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    l1=l+2*w;
+    b1=b+2*w;
+    f=((l1*b1)-(l*b))*c;
+   if(l1<=0||b1<=0)
+   {
+   printf("impossible");
+   }
+   else
+   {
+   printf("%d",f);
+   }
+    return 0;
+    
+    
+}
