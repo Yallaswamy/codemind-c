fcceb737ba85ca50a143e9b77538d1f290fbb400
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-05-02 15:00:28 +0530

    #include<stdio.h> int main() { 	int n,m,r,s,p; 	scanf("%d",&n); 	m=n*n; 	//while(m=0) 	{ 		r=m%10; 		s=m%100; 		p=m%1000; 	    //m=m/10; 	} 	if(n==r||n==s||p==n) 	{ 		printf("Automorphic Number"); 	} 	else 	{ 		printf("Not an Automorphic Number"); 	} }

---
+++ Automorphic_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,r,s,p;
+	scanf("%d",&n);
+	m=n*n;
+	//while(m=0)
+	{
+		r=m%10;
+		s=m%100;
+		p=m%1000;
+	    //m=m/10;
+	}
+	if(n==r||n==s||p==n)
+	{
+		printf("Automorphic Number");
+	}
+	else
+	{
+		printf("Not an Automorphic Number");
+	}
+}
