54ba74829a6981d32dc54edb6dd2a6a4ffa1e183
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-05-06 22:35:14 +0530

    #include <stdio.h> int main() {     int n1, n2, i, gcd;
        scanf("%d %d", &n1, &n2);
    
        for(i=1; i <= n1 && i <= n2; ++i)//4 6
        {
            // Checks if i is factor of both integers
            if(n1%i==0 && n2%i==0)
                gcd = i;
        }
    
        printf("%d",gcd);
    
        return 0;
    }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n1,n2,gcd,i;
-    scanf("%d%d",&n1,&n2);
-    for(i=1;i<=n1 && i<=n2;i++)
-    {
-        if(n1%i==0&&n2%i==0)
-        {
-            gcd=i;
-        }
-    }
-    printf("%d",gcd);
-    return 0;
+    int n1, n2, i, gcd;
+
     
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)//4 6
+    {
+        // Checks if i is factor of both integers
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
+    }
+
+    printf("%d",gcd);
+
+    return 0;
 }
