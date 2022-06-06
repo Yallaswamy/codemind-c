e0febb8f3796e344976c00c7f831822e730501f7
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-06 08:33:09 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[100],i,m=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0 && a[i]%2==0)         {             m=1;         }     }     if(m==1)     {         printf("False");     }     else     {         printf("True");     } }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -3,32 +3,24 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i,f=0;
+    int a[100],i,m=0;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2!=0)
+        if(i%2!=0 && a[i]%2==0)
         {
-            if(i%2==0)
-            {
-                f=1;
-                break;
-            }
+            m=1;
         }
     }
-    if(f==0)
+    if(m==1)
     {
-        printf("True");
+        printf("False");
     }
     else
     {
-        printf("False");
+        printf("True");
     }
 }
